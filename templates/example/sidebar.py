6a075abf3a3249e4b28cfae8dd67d5eb4fd9e837
SanthoshSachin <87323619+SANTHOSH-SACHIN@users.noreply.github.com>
2024-07-09 11:38:34 +0530

    Initial commit --release 1.0

---
+++ templates/example/sidebar.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+def show():
+    
+    
+    inputs = {}  # dict to store all user inputs until return
+    
+    with st.sidebar:
+        
+       
+        st.write("## Model")
+        
+        inputs["model"] = st.selectbox("Which model?", ["Top model", "Role model"])
+        st.write("You should probably finish this... ;)")
+        
+    return inputs
+
+
+
+if __name__ == "__main__":
+    show()
+    
+    
